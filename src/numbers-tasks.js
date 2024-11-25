c5a0f8719a4b212e95e551f73c21882f02a43e90
Danil <dd.wasabully@gmail.com>
2024-11-25 17:33:21 +0300

    done isPowerOfTwo

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -292,8 +292,11 @@ const getSumOfDigits = (num) => {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  for (let i = 1; i <= num; i *= 2) {
+    if (i == num) return true;
+  }
+  return false;
 }
 
 /**
